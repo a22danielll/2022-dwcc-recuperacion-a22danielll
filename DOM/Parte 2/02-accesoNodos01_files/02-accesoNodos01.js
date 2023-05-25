a8d5fab32bec29ba63a86063f4d9e490fafb8b64
a22danielll <a22danielll@iessanclemente.net>
2023-05-25 14:12:53 +0200

    s

---
+++ DOM/Parte 2/02-accesoNodos01_files/02-accesoNodos01.js
@@ -0,0 +1,7 @@
+var ageTable = document.getElementById("age-table");
+var labels = ageTable.getElementsByTagName("label");
+var firstTd = ageTable.querySelector("td");
+var searchForm = document.querySelector('form[name="search"]');
+var firstInput = searchForm.querySelector("input");
+var formInputs = searchForm.getElementsByTagName("input");
+var lastInput = formInputs[formInputs.length - 1];
