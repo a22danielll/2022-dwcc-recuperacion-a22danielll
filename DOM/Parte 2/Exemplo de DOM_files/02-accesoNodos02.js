333b31006af4ee4dcd8d8a5ed4b6984646f1e697
a22danielll <a22danielll@sanclemente.net>
2023-05-25 22:11:29 +0200

    s

---
+++ DOM/Parte 2/Exemplo de DOM_files/02-accesoNodos02.js
@@ -0,0 +1,15 @@
+let input2 = document.getElementById('input2');
+console.log(input2);
+let paragrafos = document.getElementsByTagName('p');
+console.log(paragrafos);
+let lipsum =  document.getElementById('lipsum');//div
+let paragrafos2 = lipsum.getElementsByTagName('p');
+console.log(paragrafos2);
+let formulario = document.getElementsByTagName('form')[0];
+console.log(formulario);
+let inputs = document.getElementsByTagName('input');
+console.log(inputs);
+let inputs2 = document.querySelectorAll("input[name='sexo']");
+console.log(input2);
+let items = document.querySelectorAll('li.important');
+console.log(items);
