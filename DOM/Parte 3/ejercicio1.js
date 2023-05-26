ba02cc3383dc4b947d8745ced29669830b7f5450
a22danielll <a22danielll@sanclemente.net>
2023-05-26 18:49:40 +0200

    s

---
+++ DOM/Parte 3/ejercicio1.js
@@ -0,0 +1,9 @@
+
+  console.log('Imaxinar que a variable elemento fai referencia a un elemento do DOM e text é\n' + 
+  'unha variable con unha cadea de texto que inclúe etiquetas HTML. \n¿Cales dos' + 
+  'seguintes comandos farán exactamente o mesmo?');
+  
+  console.log('a. elemento.append(document.createTextNode(text)\n');
+  console.log('b. elemento.innerHTML = text\n'); 
+  console.log('c. elemento.textContent = text\n');
+
