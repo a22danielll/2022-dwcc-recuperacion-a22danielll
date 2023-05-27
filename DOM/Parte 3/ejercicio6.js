0688fb21958f3fd39d7ee959487a7f01f165d15b
a22danielll <a22danielll@sanclemente.net>
2023-05-27 19:38:20 +0200

    s

---
+++ DOM/Parte 3/ejercicio6.js
@@ -0,0 +1,23 @@
+function createTree(data) {
+    let ul = document.createElement('ul');
+    for(let key of data){
+        let li = document.createElement('li');
+        
+    }
+}
+let arbore = {
+  Fish: {
+    trout: {},
+    salmon: {},
+  },
+  Tree: {
+    Huge: {
+      sequoia: {},
+      oak: {},
+    },
+    Flowering: {
+      "apple tree": {},
+      magnolia: {},
+    },
+  },
+};
